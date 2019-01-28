719a4ba7181f1ff5afcc63e450493dffea5f290f
pzh <1972508489@qq.com>
2019-01-28 22:22:54 +0800

    Add files via upload

---
+++ Elgamel/example.py
@@ -0,0 +1,22 @@
+import Elgamel as El
+
+if __name__ == "__main__":
+    #接收方B初始化
+    B = El.Elgamel()
+    q, a, Y = B.receiveClient()
+
+    #发送方A初始化
+    A = El.Elgamel()
+    A.sendClient(q,a,Y)
+    print("共享参数q = ",q)
+    print("共享参数a = ",a)
+    print("共享参数Y = ",Y)
+
+    #发送方进行加密
+    plain = int(input("请输入明文(以十进制形式数字输入):"))
+    crpt = A.encrypt(plain)
+    print("发送方A加密得到的密文为: ", crpt)
+
+    #接收方进行解密
+    plain = B.decipher(crpt)
+    print("接收方B解密得到的明文为: ", plain)
